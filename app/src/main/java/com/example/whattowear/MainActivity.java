cef35540f95931d432a3af6aadee3369d9fc9129
nayeon0731 <55181654+nayeon0731@users.noreply.github.com>
2021-05-27 21:03:23 +0900

    icon추가, 화면 생성

--- app/src/main/java/com/example/whattowear/MainActivity.java
+++ app/src/main/java/com/example/whattowear/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.whattowear;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton searchButton = (ImageButton)findViewById(R.id.searchButton);
+        ImageButton settingButton = (ImageButton)findViewById(R.id.settingButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/whattowear/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.whattowear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+    }
+}
