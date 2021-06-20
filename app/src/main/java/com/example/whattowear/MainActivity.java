7774bda0834220ce7ebf21f18612b57dc6eb7f22
nayeon0731 <toothless1596@gmail.com>
2021-06-21 02:07:31 +0900

    SettingActivity 글꼴 수정

--- app/src/main/java/com/example/whattowear/MainActivity.java
+++ app/src/main/java/com/example/whattowear/MainActivity.java
@@ -55,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<WeatherInfoData> itemViewArrayList = new ArrayList<WeatherInfoData>();
 
 
-
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
